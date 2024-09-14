7a863961a581411f8c35cea2beea2a889067c05a
natnaelat <149197556+natnaelat@users.noreply.github.com>
2024-09-14 11:25:17 -0400

    ApplicationStarter

---
+++ java/ApplicationStarter.java
@@ -0,0 +1,10 @@
+package com.mongodb.starter;
+
+
+
+@SpringBootApplication 
+public class ApplicationStarter {
+    public static void main(String[] arg) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+}
