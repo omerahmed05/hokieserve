76289e0aa040ef1971fdd6eed07a8a80307c784c
Nathnael Mecuria <130406421+nathnaelm@users.noreply.github.com>
2024-09-15 01:38:34 -0400

    major changes to help conceptually + changes to explore aswell

--- react_project/app/(tabs)/explore.tsx
+++ react_project/app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -6,8 +6,8 @@ import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   const text = "HokieServe";
-  const angles = [-25, -20, -25, -1, 0, 5, 10, 15, 20, 25];
-  const translations = [-60, -50, -40, -30, 0, 20, 30, 40, 50, 60];
+  const angles = [-25, -20, -15, -10, 0, 5, 10, 15, 20, 25];
+  const translations = [-60, -50, -40, -30, -10, 20, 30, 40, 50, 60];
 
   const curvedText = text.split('').map((char, index) => (
     <Text
@@ -42,15 +42,15 @@ export default function HomeScreen() {
 
           {/* Request Service text and input */}
           <View style={styles.requestServiceContainer}>
-            <TouchableOpacity style={styles.serviceButton} onPress={() => console.log('Search button pressed')}>
+            {/* <TouchableOpacity style={styles.serviceButton} onPress={() => console.log('Search button pressed')}>
             <ThemedText type="title" style={styles.requestServiceText}>Request Service</ThemedText></TouchableOpacity>
-            <Text style={styles.subText}>Submit a Help Request Using the Button Above:</Text>
+            <Text style={styles.subText}>Submit a Help Request Using the Button Above:</Text> */}
             
             {/* Container for the search bar and button */}
             <View style={styles.searchContainer}>
               <TextInput
                 style={styles.searchBar}
-                placeholder="Search for services you would like to offer!"
+                placeholder="Search for services to offer others!"
               />
               <TouchableOpacity style={styles.searchButton} onPress={() => console.log('Search button pressed')}>
                 <Text style={styles.buttonText}>Search</Text>
@@ -78,7 +78,7 @@ const styles = StyleSheet.create({
     marginBottom: 30,
   },
   HokieServeTxt: {
-    fontSize: 40,
+    fontSize: 30,
     top: 10,
     fontWeight: 'bold',
     color: '#FF6600',
@@ -86,16 +86,7 @@ const styles = StyleSheet.create({
     textShadowOffset: { width: -1, height: 1 },
     textShadowRadius: 10,
   },
-  // Profile button styling
-  // profileButton: {
-  //   position: 'absolute',
-  //   top: 10,
-  //   right: 10,
-  //   padding: 10,
-  //   backgroundColor: '#FF6600',
-  //   borderRadius: 5,
-  //   zIndex: 1,
-  // },
+
   profileText: {
     color: '#FFF',
     fontWeight: 'bold',
@@ -129,13 +120,15 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     marginRight: 10, // Space between the search bar and the button
-    width: 300,
+    width: 285,
+    marginTop: 10 
   },
   searchButton: {
     backgroundColor: '#861F41',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
+    marginTop: 10 
   },
   buttonText: {
     color: '#FFF',
